0e71758e643bfad9682d6f30791cc8b47242bb64
Lcat Lin <xiong.lin@heysroad.com>
2016-05-11 11:00:22 +0800

    init imgview

---
+++ README.md
@@ -0,0 +1,12 @@
+> 图片弹窗预览
+
+### 使用方法
+
+```
+<a class="view" href="" data-src="http://path/to.jpg">查看范本</a>
+
+$('.view').imgView({
+  title: '查看范本', // 默认值： view
+  width: 1000 // 默认值： 800
+})
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+var $ = require('jquery');
+var dialog = require('artdialog');
+
+$.fn.extend({
+  /**
+   * 图片弹窗预览
+   * @param  {Object} options 参数
+   * @return {Object}         返回弹窗实例
+   */
+  imgView: function(options) {
+    var self = $(this);
+
+    self.on('click', function(e) {
+      e.preventDefault();
+      var width = options.width || 800;
+      var maxWidth = width - 20;
+      // fixed: 每个浏览器高度不一致，不能定高
+      var height = $(window).height() - 100;
+      var maxHeight = height - 50;
+      var el = $(e.target);
+      var url = el.attr('src') || el.attr('data-src');
+      if (url.indexOf('@') >= 0) {
+        var index = url.search(/@/);
+        url = url.substr(0, index);
+      }
+
+      var opt = $.extend({
+        title: 'view',
+        fixed: true,
+        content: '<div class="hbox"><div class="col v-middle text-center-i"><img style="max-width:' + maxWidth + 'px;max-height:' + maxHeight + 'px;" src="' + url + '" alt="" /></div></div>'
+      }, options || {})
+      var _dialog = dialog(opt)
+      .width(width)
+      .height(height)
+      .showModal()
+      return _dialog;
+    })
+  }
+})
